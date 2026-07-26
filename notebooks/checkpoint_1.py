21b169a2fdbec501e1a4b794afad0ba5c9bcfe09
Gulshan Raj Shetty <gulshanraj@gmail.com>
2026-07-26 14:34:44 -0400

    Updated Return From Checkpoint-1 to Include Task Callout.

--- notebooks/checkpoint_1.py
+++ notebooks/checkpoint_1.py
@@ -69,7 +69,7 @@ def _(mo):
             kind="info",
         )
 
-    return print_sections, task_callout
+    return print_sections, task_callout # Return the utility functions for use in other cells
 
 
 @app.cell(hide_code=True)
